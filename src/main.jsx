d2a44e407c361f78e867b80bb0caed60fd3aa1a3
trungdung70199 <trungdung70199@gmail.com>
2023-12-26 16:34:45 +0900

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {
   creatBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import './index.css'
+import './index.css';
+
+const router = creatBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World!</div>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+   <RouterProvider router={router} /> 
   </React.StrictMode>,
 )
