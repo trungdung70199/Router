14f729ba8f9b469cbce7f92fe485c73299aaaa6d
trungdung70199 <trungdung70199@gmail.com>
2023-12-27 00:48:27 +0900

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,14 @@ import './index.css';
 import Root from './routes/root';
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
-import Root, { loader as rootLoader } from "./routes/root";
+// import Root, { loader as rootLoader } from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root/>,
     errorElement: <ErrorPage/>,
-    loader: rootLoader,
+    // loader: rootLoader,
     children: [
       {
         path: "contacts/:contactId",
