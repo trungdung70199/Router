41e05ea0930a20b102d8f205d081b59d9172d708
trungdung70199 <trungdung70199@gmail.com>
2023-12-27 00:37:00 +0900

    update main root

--- src/main.jsx
+++ src/main.jsx
@@ -14,11 +14,17 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root/>,
     errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact/>,
+      }
+    ]
   },
-  {
-    path: "Contacts/:contactId",
-    element: <Contact />,
-  }
+  // {
+  //   path: "Contacts/:contactId",
+  //   element: <Contact />,
+  // }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
