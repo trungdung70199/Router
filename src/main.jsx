e288ae79748b988efdc289eb9bb3f675fbf37f31
trungdung70199 <trungdung70199@gmail.com>
2023-12-27 18:35:08 +0900

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,27 +5,21 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css';
+import Root from './routes/root';
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
-import Root, { loader as rootLoader } from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>,
+    element: <Root />,
     errorElement: <ErrorPage/>,
-    loader: rootLoader,
-    children: [
-      {
-        path: "contacts/:contactId",
-        element: <Contact/>,
-      },
-    ],
+    // loader: rootLoader,
   },
-  // {
-  //   path: "Contacts/:contactId",
-  //   element: <Contact />,
-  // }
+  {
+    path: "contacts/:contactId",
+    element: <Contact/>,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
