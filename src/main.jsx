4aa713caad67247ab1de7805c21e3d0d51c295d1
trungdung70199 <trungdung70199@gmail.com>
2023-12-27 18:40:56 +0900

    update main root

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,12 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage/>,
-    // loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
   {
     path: "contacts/:contactId",
