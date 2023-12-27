8f6d37441f400bad5b50ed9bb669db30f06f468e
trungdung70199 <trungdung70199@gmail.com>
2023-12-27 13:21:29 +0900

    update root

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -61,14 +61,7 @@ export default function Root() {
                             <i>No contacts</i>
                         </p>
                     )}
-                    {/* <ul>
-                        <li>
-                            <Link to={`/contacts/1`}>Your Name</Link>
-                        </li>
-                        <li>
-                            <Link to={`/contacts/2`}>Your Friend</Link>
-                        </li>
-                    </ul> */}
+                    
                 </nav>
             </div>
             <div id="detail">
