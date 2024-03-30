26148f5a102fb6641496b0cf4f0317d1544b3cb1
trundung70199 <trungdung70199@gmail.com>
2024-03-30 17:37:07 +0900

    update children

--- src/main.jsx
+++ src/main.jsx
@@ -11,11 +11,17 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact/>,
+      },
+    ],
   },
-  {
-    path: "contacts/:contactId",
-    element: <Contact />,
-  },
+  // {
+  //   path: "contacts/:contactId",
+  //   element: <Contact />,
+  // },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
