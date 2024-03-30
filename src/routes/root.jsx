3206b143562075ae2e4fdf544a4270a2b7c241d6
trundung70199 <trungdung70199@gmail.com>
2024-03-30 17:17:25 +0900

    update client side routing use Link

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,3 +1,5 @@
+import { Outlet, Link } from "react-router-dom";
+
 export default function Root() {
   return (
     <>
@@ -22,15 +24,19 @@ export default function Root() {
         <nav>
           <ul>
             <li>
-              <a href={`/contacts/1`}>Your Name</a>
+              {/* <a href={`/contacts/1`}>Your Name</a> */}
+              <Link to={`contacts/1`}>Your Name</Link>
             </li>
             <li>
-              <a href={`/contacts/2`}>Your Friend</a>
+              {/* <a href={`/contacts/2`}>Your Friend</a> */}
+              <Link to={`contacts/2`} >Your Name</Link>
             </li>
           </ul>
         </nav>
       </div>
-      <div id="detail"></div>
+      <div id="detail">
+        <Outlet/>
+      </div>
     </>
   );
 }
